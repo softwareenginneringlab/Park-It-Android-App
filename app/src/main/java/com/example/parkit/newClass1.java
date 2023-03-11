73471adf762f5775aa46d37b54da835ddcbcbd67
abrar <mahirabrar075@gmail.com>
2023-03-11 10:50:54 +0600

    new java class added

---
+++ app/src/main/java/com/example/parkit/newClass1.java
@@ -0,0 +1,26 @@
+package com.example.parkit;
+
+public class newClass1 {
+    public int a,b;
+
+    public newClass1() {
+
+    }
+    void setterA(int a)
+    {
+        this.a = a;
+    }
+    void setterB(int b)
+    {
+        this.b = b;
+    }
+    int getterA()
+    {
+        return a;
+    }
+    int getterb()
+    {
+        return b;
+    }
+}
+
