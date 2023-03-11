2db5e2cef58f0ee935f408e368fd89365e7d3fd9
abrar <mahirabrar075@gmail.com>
2023-03-11 15:30:29 +0600

    conflict created

---
+++ app/src/main/java/com/example/parkit/conflictClass.java
@@ -0,0 +1,28 @@
+package com.example.parkit;
+
+public class conflictClass {
+
+        public int a,b;
+
+        public conflictClass() {
+
+        }
+        void setterA(int a)
+        {
+            this.a = a;
+        }
+        void setterB(int b)
+        {
+            this.b = b;
+        }
+        int getterA()
+        {
+            return a;
+        }
+        int getterb()
+        {
+            return b;
+        }
+
+
+}
