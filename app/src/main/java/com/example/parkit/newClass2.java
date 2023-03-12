4c59be79932e102d23a0a4eb7e66e3ef191ea6e3
abrar <mahirabrar075@gmail.com>
2023-03-12 08:36:37 +0600

    new file generated

---
+++ app/src/main/java/com/example/parkit/newClass2.java
@@ -0,0 +1,26 @@
+package com.example.parkit;
+
+public class newClass2 {
+    public int a,c;
+
+    public newClass2() {
+
+    }
+    void setterA(int a)
+    {
+        this.a = a;
+    }
+    void setterB(int b)
+    {
+        this.c = b;
+    }
+    int getterA()
+    {
+        return a;
+    }
+    int getterb()
+    {
+        return c;
+    }
+}
+
